d1e48d6a652fed235f3a1b592a919ae1ade4caf8
imalgrab <igormalgrab@gmail.com>
2020-04-13 16:05:23 +0200

    zad5

--- SI/lista02/zad5.py
+++ SI/lista02/zad5.py
@@ -1,5 +1,4 @@
-import queue
-import random
+import heapq
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, 1, -1]
@@ -7,13 +6,24 @@ mv = {0: 'U', 1: 'D', 2: 'R', 3: 'L'}
 
 walls = set()
 goals = set()
-visited = set()
 
 
 class state:
     def __init__(self, positions, moves):
         self.positions = positions
         self.moves = moves
+
+
+def len_from_goals(s):
+    sum_dist = 0
+    for p in s.positions:
+        min_dist = 1e9
+        for g in goals:
+            new_dist = abs(g[0] - p[0]) + abs(g[1] - p[1])
+            if new_dist < min_dist:
+                min_dist = new_dist
+        sum_dist += min_dist
+    return sum_dist
 
 
 def is_final(s):
@@ -29,14 +39,12 @@ def move(s, d):
     for pos in s.positions:
         x = pos[0] + dx[d]
         y = pos[1] + dy[d]
-        new_pos = (x, y)
-        if new_pos not in walls:
-            ps.add(new_pos)
+        if (x, y) not in walls:
+            ps.add((x, y))
         else:
             ps.add(pos)
     ms += mv[d]
-    new_s = state(ps, s.moves+ms)
-    return new_s
+    return state(ps, s.moves+ms)
 
 
 def moves(s, n, d):
@@ -82,33 +90,26 @@ def get_adj(s):
 
 
 def bfs(s):
-    shortest = len(s.positions)
-    q = queue.Queue()
-    q.put(s)
+    visited = set()
+    q = []
+    heapq.heappush(q, (len_from_goals(s), s))
     visited.add(tuple(s.positions))
-    while not q.empty():
-        curr = q.get()
+    while len(q) > 0:
+        curr = heapq.heappop(q)[1]
         if is_final(curr):
             return curr.moves
-        adj = get_adj(curr)
-        for adj_state in adj:
-            if tuple(adj_state.positions) not in visited:
-                if len(adj_state.positions) < shortest:
-                    visited.clear()
-                    q.queue.clear()
-                    shortest = len(adj_state.positions)
-                visited.add(tuple(adj_state.positions))
-                q.put(adj_state)
+        adjs = get_adj(curr)
+        for adj in adjs:
+            if tuple(adj.positions) not in visited:
+                visited.add(tuple(adj.positions))
+                heapq.heappush(q, (len_from_goals(adj), adj))
 
 
 def start(data):
     goals.clear()
     walls.clear()
-    visited.clear()
     init_state = init(data)
-    mid_state = moves(init_state, len(data)-2, 1)
-    mid_state = moves(mid_state, len(data[0])-2, 3)
-    final_state = bfs(mid_state)
+    final_state = bfs(init_state)
     out.write(final_state)
     out.write('\n')
 
